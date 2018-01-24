b3d5964c49ad652a311a1ed53e4e7564575d2dcc
Niklaus Zbinden <niklaus@contovista.com>
2018-01-24 20:36:43 +0100

    INIT

---
+++ src/main/java/com/tim/projectnode/ProjectnodeApplication.java
@@ -0,0 +1,12 @@
+package com.tim.projectnode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectnodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectnodeApplication.class, args);
+	}
+}
